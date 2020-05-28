2f724feda277bb86d0d5ea7a32bdfeb204e2ce5d
CDA-quinton-alice <61801604+CDA-quinton-alice@users.noreply.github.com>
2020-05-28 12:13:40 +0200

    Test de modification avec un petit syso Alice

--- cmd-project/CmdV2/src/Program.java
+++ cmd-project/CmdV2/src/Program.java
@@ -5,7 +5,7 @@ public class Program {
 		System.out.println("Null");
 		System.out.println("Ok pour moi Mathieu");
 		System.out.println("Ok pour moi Samuel");
-
+		System.out.println("Ok pour moi Alice");
 	}
 
 }
