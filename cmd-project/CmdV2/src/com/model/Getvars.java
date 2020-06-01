93c0e7044705e401a60b9c637110f8fe4ab3d5c0
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-06-01 13:39:49 +0200

    Ajout GETVARS -env - prop + indentation command cd

---
+++ cmd-project/CmdV2/src/com/model/Getvars.java
@@ -0,0 +1,55 @@
+package com.model;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Getvars extends Command {
+
+	public Getvars() {
+		super("getvars");
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean execute() {
+		Map<String, String> mapEnv = System.getenv();
+		for (Entry<String, String> mapEntry : mapEnv.entrySet()) {
+			System.out.println(mapEntry.getKey() + "\t\t\t" + mapEntry.getValue());
+		}
+		System.out.println("--------------------------------------------------------------");
+		Map<Object, Object> mapProp = System.getProperties();
+		for (Entry<Object, Object> mapEntry : mapProp.entrySet()) {
+			System.out.println(mapEntry.getKey() + "\t\t\t" + mapEntry.getValue());
+		}
+		return true;
+	}
+
+	@Override
+	public boolean execute(ArrayList<String> args) {
+		String vParam = args.get(0);
+		if (vParam.equalsIgnoreCase("-env")) {
+			Map<String, String> mapEnv = System.getenv();
+			for (Entry<String, String> mapEntry : mapEnv.entrySet()) {
+				System.out.println(mapEntry.getKey() + "\t\t\t" + mapEntry.getValue());
+			}
+
+		} else if (vParam.equalsIgnoreCase("-prop")) {
+			Map<Object, Object> mapProp = System.getProperties();
+			for (Entry<Object, Object> mapEntry : mapProp.entrySet()) {
+				System.out.println(mapEntry.getKey() + "\t\t\t" + mapEntry.getValue());
+			}
+		} else {
+			System.out.println("Paramètre inconnu");
+		}
+		return true;
+	}
+
+	@Override
+	public void describe() {
+		System.out.println("getvars : Affiche les variables d'environnement et les propriétés de la JVM.");
+		System.out.println("getvars [-env] : Affiche les variables d'environnement.");
+		System.out.println("getvars [-prop] : Affiche les propriétés de la JVM.");
+	}
+
+}
