13ed4fe5195f6e19679c3d0d3ae802a2f4a0dd63
Mathieu AMBROISE <mat.ambroise@gmail.com>
2020-06-01 17:19:05 +0200

    Modification de HELP maintenant fonctionne avec et sans paramètre
    Modif classe CmdV2  et indentation auto

--- cmd-project/CmdV2/src/com/model/Command.java
+++ cmd-project/CmdV2/src/com/model/Command.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 
 public abstract class Command {
 	protected String vNomCommand;
-	
+
 	public Command(String vNomCommand) {
 		this.vNomCommand = vNomCommand;
 	}
-	 
+
 	public abstract boolean execute();
+
 	public abstract boolean execute(ArrayList<String> args);
+
 	public abstract void describe();
 
 	public String getvNomCommand() {
 		return vNomCommand;
 	}
-	
+
 	public String toString() {
 		return this.vNomCommand;
 	}
--- cmd-project/CmdV2/src/com/model/River.java
+++ cmd-project/CmdV2/src/com/model/River.java
@@ -9,46 +9,42 @@ public class River extends Command {
 		super("river");
 	}
 
-@Override
-public boolean execute(ArrayList<String> args) {
-	{
-		System.out.println("Saisissez votre première rivière numérique");
-        Scanner in = new Scanner(System.in);
-        long r1 = in.nextLong();
-        System.out.println("Saisissez votre seconde rivière numérique");
-        long r2 = in.nextLong();
-        while (r1 != r2) {
-            while (r1 < r2) {
-                r1 = nextValue(r1);
-            }
-            while (r1 > r2) {
-                r2 = nextValue(r2);
-            }
-        }
-        System.out.println("première intersection des rivières numériques obtenues pour ces paramètres : "+r1);
-    }
+	@Override
+	public boolean execute(ArrayList<String> args) {
+		{
+			System.out.println("Saisissez votre première rivière numérique");
+			Scanner in = new Scanner(System.in);
+			long r1 = in.nextLong();
+			System.out.println("Saisissez votre seconde rivière numérique");
+			long r2 = in.nextLong();
+			while (r1 != r2) {
+				while (r1 < r2) {
+					r1 = nextValue(r1);
+				}
+				while (r1 > r2) {
+					r2 = nextValue(r2);
+				}
+			}
+			System.out.println("première intersection des rivières numériques obtenues pour ces paramètres : " + r1);
+		}
 
-    
+		return true;
 
-	return true;
-
-}
-
-static long nextValue(long value) {
-	for (char c : String.valueOf(value).toCharArray()) {
-		value += c -'0';
 	}
-	return value;
-}
 
+	static long nextValue(long value) {
+		for (char c : String.valueOf(value).toCharArray()) {
+			value += c - '0';
+		}
+		return value;
+	}
 
+	@Override
+	public void describe() {
+		System.out.println(
+				"river [param1 param2] : prend 2 paramètres en entrée puis affiche la première intersection des rivières numériques obtenues pour ces derniers.");
 
-
-@Override
-public void describe() {
-System.out.println("[river] : prend 2 paramètres en entrée puis affiche la première intersection des rivières numériques obtenues pour ces derniers.");
-	
-}
+	}
 
 	@Override
 	public boolean execute() {
