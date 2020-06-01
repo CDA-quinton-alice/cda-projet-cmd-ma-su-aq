365daab29cafcae2eb44f7f50960d695643226a1
Samuel-umek <samuel.umek@gmail.com>
2020-06-01 16:44:58 +0200

    Correction de bug de Crd ?

--- cmd-project/CmdV2/src/com/manager/CmdV2.java
+++ cmd-project/CmdV2/src/com/manager/CmdV2.java
@@ -13,19 +13,18 @@ public class CmdV2 {
 	public CmdV2() {
 		pwd = System.getProperty("user.dir");
 		vCommands = new ArrayList<>();
-		File dir = new File(pwd+"\\src\\com\\model");
+		File dir = new File(pwd + "\\src\\com\\model");
 		String s[] = dir.list();
 		String s2[] = new String[s.length];
-		for (int z=0;z<s.length;z++) {
-				s2[z] = s[z].substring(0, s[z].lastIndexOf("."));
-			
-		}
+		for (int z = 0; z < s.length; z++) {
+			s2[z] = s[z].substring(0, s[z].lastIndexOf("."));
 
+		}
 		try {
-			for (int i=0;i<s2.length;i++) {
+			for (int i = 0; i < s2.length; i++) {
 				String classe = s2[i];
-				Class<?> cls = Class.forName("com.model."+classe);
-				if(Command.class.isAssignableFrom(cls)&&!(cls.getName().toString().equals("com.model.Command"))) {
+				Class<?> cls = Class.forName("com.model." + classe);
+				if (Command.class.isAssignableFrom(cls) && !(cls.getName().toString().equals("com.model.Command"))) {
 					try {
 						Command c = (Command) cls.newInstance();
 						vCommands.add(c);
@@ -34,8 +33,8 @@ public class CmdV2 {
 					}
 				}
 			}
-		}catch(ClassNotFoundException cnf) {
-			System.out.println("C");
+		} catch (ClassNotFoundException cnf) {
+			System.out.println(cnf.toString());
 			cnf.getStackTrace();
 		}
 	}
@@ -54,13 +53,12 @@ public class CmdV2 {
 		CmdV2.pwd = pwd;
 	}
 
-	
 	public boolean execute(String pCommande, ArrayList<String> pListeArgs) {
 		Command command = getCommandByName(pCommande);
-		if(command !=null) {
-			((History)getCommandByName("history")).ajouterCommande(pCommande, pListeArgs);
+		if (command != null) {
+			((History) getCommandByName("history")).ajouterCommande(pCommande, pListeArgs);
 			return command.execute(pListeArgs);
-		}else {
+		} else {
 			System.out.println("La commande n'existe pas !");
 		}
 		return true;
@@ -68,18 +66,18 @@ public class CmdV2 {
 
 	public boolean execute(String pCommande) {
 		Command command = getCommandByName(pCommande);
-		if(command !=null) {
-			((History)getCommandByName("history")).ajouterCommande(pCommande, null);
+		if (command != null) {
+			((History) getCommandByName("history")).ajouterCommande(pCommande, null);
 			return command.execute();
-		}else {
+		} else {
 			System.out.println("La commande n'existe pas !");
 		}
 		return true;
 	}
-	
+
 	public static Command getCommandByName(String pCmd) {
 		for (Command command : vCommands) {
-			if(command.getvNomCommand().equalsIgnoreCase(pCmd)) {
+			if (command.getvNomCommand().equalsIgnoreCase(pCmd)) {
 				return command;
 			}
 		}
