06326185339eb369e850c961f0e69525ccbe316e
Samuel-umek <samuel.umek@gmail.com>
2020-06-02 12:09:01 +0200

    Correction duplication fichier Find.java

--- cmd-project/CmdV2/src/com/model/Find.java
+++ cmd-project/CmdV2/src/com/model/Find.java
@@ -1,8 +1,11 @@
 package com.model;
 
+import java.io.File;
 import java.util.ArrayList;
 
-public class Find extends Command{
+import com.manager.CmdV2;
+
+public class Find extends Command {
 
 	public Find() {
 		super("find");
@@ -11,20 +14,73 @@ public class Find extends Command{
 
 	@Override
 	public boolean execute() {
-		// TODO Auto-generated method stub
+		this.describe();
 		return true;
 	}
 
 	@Override
 	public boolean execute(ArrayList<String> args) {
-		// TODO Auto-generated method stub
+		String pwd = CmdV2.getPwd();
+		File dir = new File(pwd);
+		String s[] = dir.list();
+
+		String start = null;
+		String end = null;
+		String exact = null;
+		ArrayList<String> answer = new ArrayList<>();
+
+		boolean isStart = false;
+		boolean isEnd = false;
+		for (String str : args) {
+
+			// On s'occupe de récupérer ce qui a été placé en argument
+			if (isStart) {
+				start = str;
+				isStart = false;
+			} else if (isEnd) {
+				end = str;
+				isEnd = false;
+			} else if (str.equalsIgnoreCase("-start")) {
+				isStart = true;
+			} else if (str.equalsIgnoreCase("-end")) {
+				isEnd = true;
+			} else {
+				exact = str;
+			}
+		}
+		for (int i = 0; i < s.length; i++) {
+			String curr = s[i].toLowerCase();
+			if (start != null) {
+				if (curr.matches("^" + start + ".*")) {
+					answer.add(s[i]);
+				}
+			} else if (end != null) {
+				if (curr.matches(".*" + end + "$")) {
+					answer.add(s[i]);
+				}
+			} else {
+				if (curr.matches("^" + exact + "$")) {
+					answer.add(s[i]);
+				}
+			}
+
+		}
+
+		if (answer.size() > 0) {
+			answer.stream().forEach(System.out::println);
+		} else {
+			System.out.println("Aucune occurence trouvé.");
+		}
+
 		return true;
 	}
 
 	@Override
 	public void describe() {
-		System.out.println("find [-start] [-end] : recherche parmis le dossier courant les fichiers ");
-		
+		System.out.println(
+				"find [-start] [-end] [param] : Cherche dans le répertoire courant des fichiers commençant et/ou \n"
+						+ "terminant le nom spécifié en paramètre ou cherche l'exacte terminologie sinon.");
+
 	}
 
 }
